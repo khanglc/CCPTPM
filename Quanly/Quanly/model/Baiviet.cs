4e2d0f8f83ed89b2eff585e912203fe6af0d8d4e
khanglc <91674593+khanglc@users.noreply.github.com>
2021-11-24 09:29:52 +0700

    update

---
+++ Quanly/Quanly/model/Baiviet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quanly.model
+{
+    public class Baiviet
+    {
+        private int idtin;
+        private string tieude;
+        private string hinh;
+        private DateTime ngay;
+        private int idUser;
+        private string noidung;
+        private int trangthai;
+
+        public int Idtin { get => idtin; set => idtin = value; }
+        public string Tieude { get => tieude; set => tieude = value; }
+        public string Hinh { get => hinh; set => hinh = value; }
+        public DateTime Ngay { get => ngay; set => ngay = value; }
+        public int IdUser { get => idUser; set => idUser = value; }
+        public string Noidung { get => noidung; set => noidung = value; }
+        public int Trangthai { get => trangthai; set => trangthai = value; }
+    }
+}
---
+++ Quanly/Quanly/model/Taikhoan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quanly.model
+{
+    public class Taikhoan
+    {
+        private int id;
+        private string hoten;
+        private DateTime ngaysinh;
+        private string diachi;
+        private string email;
+        private string username;
+        private string password;
+        private int quyen;
+        private int trangthai;
+
+        public int Id { get => id; set => id = value; }
+        public string Hoten { get => hoten; set => hoten = value; }
+        public DateTime Ngaysinh { get => ngaysinh; set => ngaysinh = value; }
+        public string Diachi { get => diachi; set => diachi = value; }
+        public string Email { get => email; set => email = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public int Trangthai { get => trangthai; set => trangthai = value; }
+        public int Quyen { get => quyen; set => quyen = value; }
+    }
+}
