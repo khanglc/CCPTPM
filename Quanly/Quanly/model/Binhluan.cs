c7569df94287013691324b0c43bb7a2817059000
khanglc <91674593+khanglc@users.noreply.github.com>
2021-12-11 15:35:13 +0700

    update

---
+++ Quanly/Quanly/model/Binhluan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quanly.model
+{
+    public class Binhluan
+    {
+        private int id;
+        private int idtaikhoan;
+        private int trangthai;
+
+        public int Id { get => id; set => id = value; }
+        public int Idtaikhoan { get => idtaikhoan; set => idtaikhoan = value; }
+        public int Trangthai { get => trangthai; set => trangthai = value; }
+    }
+}
